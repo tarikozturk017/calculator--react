59dcf356c24bc2d543ffe9a975c1da0808bf95a7
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-11-05 16:45:31 -0400

    first commit

---
+++ calculator/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+      <div className="previous-operand"></div>
+      <div className="current-operand"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
